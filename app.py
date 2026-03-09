0ce6492e5407441738e41fc1532f399632a377c7
HAMDI <hamdiyahya240@gmail.com>
2026-03-09 01:33:39 +0100

    Alert in grafanna

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ from prometheus_client import start_http_server, Counter, Summary
 
 app = Flask(__name__)
 
+# --- PROMETHEUS METRICS ---
 REQUESTS = Counter('flight_search_total', 'Total flight searches')
 CACHE_HITS = Counter('flight_search_hits_total', 'Searches found in Redis')
 CACHE_MISSES = Counter('flight_search_misses_total', 'Searches NOT found in Redis')
+ERROR_COUNTER = Counter('flight_search_errors_total', 'Total failed flight searches')
 LATENCY = Summary('flight_search_duration_seconds', 'Time spent processing search')
 
 REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
@@ -34,6 +36,7 @@ def get_price():
         cache.setex(key, 60, new_price) 
         return {"route": key, "price": new_price, "source": "Legacy DB"}
     except ConnectionError:
+        ERROR_COUNTER.inc()
         return {"error": "System Degraded"}, 500
 
 if __name__ == '__main__':
